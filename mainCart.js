96c198d8b6dd6a7a76987591a0be1b256566bc5c
Sakshi Mohan Harikant <connect.sakshi16@gmail.com>
2026-03-13 19:54:18 +0530

    Update mainCart.js

--- mainCart.js
+++ mainCart.js
@@ -103,7 +103,7 @@ document.addEventListener("DOMContentLoaded", function () {
                   2
                 )}</span>
             </div>
-            <a href="checkout.html">
+            <a href="checkOut.html">
 <button class="checkout-btn">Proceed to Checkout</button>
 </a>
             <a href="products.html" class="continue-shopping">Continue Shopping</a>
@@ -184,4 +184,5 @@ document.addEventListener("DOMContentLoaded", function () {
         renderCart();
       }
     });
+
   });
